5be5407e2b91c35802a3b14b27af07ee52bfcfd2
viviach <viviana.aragonch@gmail.com>
2021-07-10 13:21:54 -0500

    js code folder

---
+++ Static/js/logic.js
@@ -0,0 +1,10 @@
+// var newYorkCoords = [40.73, -74.0059];
+// var mapZoomLevel = 12;
+
+// Create the createMap function
+var myMap = L.map("map-id", {
+  center: [40.73, -74.0059],
+  zoom: 12
+});
+
+
