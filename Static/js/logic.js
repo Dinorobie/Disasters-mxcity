21fd3682f256592c69d42aaa967321bd6b0acb8e
viviach <viviana.aragonch@gmail.com>
2021-07-20 17:30:41 -0500

    js updated

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -1,10 +1,17 @@
-// var newYorkCoords = [40.73, -74.0059];
-// var mapZoomLevel = 12;
-
-// Create the createMap function
-var myMap = L.map("map-id", {
-  center: [40.73, -74.0059],
-  zoom: 12
+/// Create the createMap function
+var myMap = L.map("map", {
+  center: [19.334955176460703, -99.19755666947482],
+  zoom: 11
 });
 
+myMap.invalidateSize();
+  // Create the tile layer that will be the background of our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 20,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
 
